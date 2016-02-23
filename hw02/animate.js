06d28eb1ff81bd07df7776819ffecc5d96aaf966
alicexue <alicex123@gmail.com>
2016-02-22 22:04:49 -0500

    completed

--- hw02/animate.js
+++ hw02/animate.js
@@ -3,37 +3,45 @@ console.log("loaded animate.js");
 var c = document.getElementById("playground");
 var ctx = c.getContext("2d");
 var circleBtn = document.getElementById("circle");
-var growing = false;
+var growing = true;
 var radius = 0;
 
-ctx.beginPath();
-ctx.lineTo(250,250);
-ctx.strokeStyle = "#4B0082";
+// create border on canvas
+ctx.strokeStyle = "#000000";
+ctx.rect(0,0,c.width,c.height);
 ctx.stroke();
 
+// create growing and shrinking circle
 var createCircle = function(e) {
-    ctx.clearRect(0,0,c.width,c.height);
+
+    // clear canvas and start new path
+    ctx.clearRect(1,1,c.width-2,c.height-2);
+    ctx.beginPath()
+    
+    // change circle radius depending on growing or not
     if (growing) {
 	radius = radius + 1;
     } else {
 	radius = radius - 1 ;
     }
+
+    // check if growing must be negated
     if (radius == c.width/2) {
 	growing = false;
-    } else {
+    } else if (radius == 0) {
 	growing = true;
     }
+
+    // draw and fill circle
     ctx.arc(c.width/2,c.height/2,radius,0,2*Math.PI);
     ctx.stroke();
     ctx.fillStyle = "#4B0082";
     ctx.fill();
-    console.log("created dot");
+    
+    //run createCircle before calling next frame
+    window.requestAnimationFrame(createCircle);
+    //console.log("changed circle radius");
+    //console.log(radius);
 }
 
-
-var circle = function(e) {
-
-}
-
-c.addEventListener("circle",createCircle);
-circleBtn.addEventListener("circle",circle);
+circleBtn.addEventListener("click",createCircle);
